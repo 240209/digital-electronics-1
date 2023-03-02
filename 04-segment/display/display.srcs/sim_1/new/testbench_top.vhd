1a16ccb47976101679fb1b6e3a498c04dbbb93d0
240209 <240209@vut.cz>
2023-03-02 14:51:34 +0100

    testbench update; stimulus process to complete

--- 04-segment/display/display.srcs/sim_1/new/testbench_top.vhd
+++ 04-segment/display/display.srcs/sim_1/new/testbench_top.vhd
@@ -32,16 +32,66 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity testbench_top is
-    Port ( LED4 : out STD_LOGIC;
-           LED5 : out STD_LOGIC;
-           LED6 : out STD_LOGIC;
-           LED7 : out STD_LOGIC;
-           SW : in STD_LOGIC_VECTOR (3 downto 0));
 end testbench_top;
 
-architecture Behavioral of testbench_top is
+------------------------------------------------------------
+-- Architecture body for testbench
+------------------------------------------------------------
+
+architecture testbench of testbench_top is
+
+  -- Testbench local signals
+  signal sig_led4 : std_logic;
+  signal sig_led5 : std_logic;
+  signal sig_led6 : std_logic;
+  signal sig_led7 : std_logic;
+  signal sig_sw   : std_logic_vector(3 downto 0);
+  signal sig_btnc : std_logic;
 
 begin
 
+  -- Connecting testbench signals with decoder entity
+  -- (Unit Under Test)
+  uut_testbench_top : entity work.top
+    port map (
+      LED(4) => sig_led4,
+      LED(5) => sig_led5,
+      LED(6) => sig_led6,
+      LED(7) => sig_led7,
+      BTNC => sig_btnc,
+      SW   => sig_sw
+    );
 
-end Behavioral;
+  --------------------------------------------------------
+  -- Data generation process
+  --------------------------------------------------------
+  p_stimulus : process is
+  begin
+
+    report "Stimulus process started";
+
+    sig_blank <= '0';    -- Normal operation
+    sig_hex   <= "0011"; -- Some default value
+    wait for 50 ns;
+
+    sig_blank <= '1';    -- Blank display
+    wait for 150 ns;
+    sig_blank <= '0';    -- Normal operation
+    wait for 15 ns;
+
+    -- Loop for all hex values
+    for ii in 0 to 15 loop
+
+      -- Convert ii decimal value to 4-bit wide binary
+      -- s_hex <= std_logic_vector(to_unsigned(ii, s_hex'length));
+      sig_hex <= std_logic_vector(to_unsigned(ii, 4));
+      wait for 50 ns;
+
+    end loop;
+
+    report "Stimulus process finished";
+    wait;
+
+  end process p_stimulus;
+
+end architecture testbench;
